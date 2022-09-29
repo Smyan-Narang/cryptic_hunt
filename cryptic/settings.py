4ffaf51b9aae10ab18246b17c2d7345bd05c7c54
Smyan Narang <68273010+Smyan-Narang@users.noreply.github.com>
2022-09-30 00:18:10 +0530

    Update settings.py

--- cryptic/settings.py
+++ cryptic/settings.py
@@ -153,7 +153,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+MEDIA_ROOT = os.path.join(BASE_DIR, "/media/")
 
 LOGIN_REDIRECT_URL = '/hunt/'
 
