ceffcd24a1742f2391f8ab17f81c0cae1c71a9bb
Smyan Narang <68273010+Smyan-Narang@users.noreply.github.com>
2022-10-01 14:18:04 +0530

    Update actions.py

--- questions/lib/actions.py
+++ questions/lib/actions.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import pytz
 
 PUBLISH_DATE = datetime(year=2022, month=9, day=18, hour=0, minute=0, second=0)
-DEACTIVATE_DATE = datetime(year=2022, month=10, day=3, hour=0, minute=0, second=0)
+DEACTIVATE_DATE = datetime(year=2022, month=10, day=3, hour=2, minute=0, second=0)
 tz = pytz.timezone('Asia/Kolkata')
 local_publish_date = tz.localize(PUBLISH_DATE)
 local_deactivate_date = tz.localize(DEACTIVATE_DATE)
